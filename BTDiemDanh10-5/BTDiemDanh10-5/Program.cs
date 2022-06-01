9cd62ba614e3b36c8e80ece6944ed6d01e54f73d
vanthanh <thanhjayz01@gmail.com>
2022-06-01 20:35:27 +0700

    sua lai bai 10/5

--- BTDiemDanh10-5/BTDiemDanh10-5/Program.cs
+++ BTDiemDanh10-5/BTDiemDanh10-5/Program.cs
@@ -4,72 +4,105 @@ namespace BTDiemDanh10_5
 {
     class Program
     {
-        public int []mang = new int[10];
+        public int[] mang = new int[11];
         public void nhap()
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine("nhap phan tu thu " + i);
                 mang[i] = int.Parse(Console.ReadLine());
-                
             }
+            xuat(mang);
         }
         public void max()
         {
             int max;
-            max = mang[0];
-            for (int i = 1; i < 10; i++)
+            max = mang[1];
+            for (int i = 1; i <= 10; i++)
             {
                 if (mang[i] > max)
                     max = mang[i];
             }
-            Console.WriteLine("max = " + max);
+            Console.Write("\n max = " + max);
         }
         public void min()
         {
             int min;
-            min = mang[0];
-            for (int i = 1; i < 10; i++)
+            min = mang[1];
+            for (int i = 1; i <= 10; i++)
             {
                 if (mang[i] < min)
                     min = mang[i];
             }
-            Console.WriteLine("min = " + min);
+            Console.Write("\n min = " + min);
+        }
+        public void XuatHienNhieu()
+        {
+            int count1 = 0, count2 = 0, vitri=1;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (count1 > count2)
+                {
+                    count2 = count1;
+                    vitri = i;
+                }
+                count1 = 0;
+                for (int j = 1; j <= 10; j++)
+                    if (mang[i] == mang[j])
+                        count1++;
+                count1--;
+            }
+            Console.Write("\n Gia tri xuat hien nhieu nhat  = {0}", mang[vitri]);
+        }
+            public void SapXepGiam()
+        {
+            int[] manggiam = new int[11];
+            manggiam = mang;
+            for (int i = 1; i< 10; i++)
+            {
+                for (int j = i + 1; j <= 10; j++)
+                    if (manggiam[i] < manggiam[j])
+                    {
+                         int tg = manggiam[i];
+                        manggiam[i] = manggiam[j];
+                        manggiam[j] = tg;
+                    } 
+            }
+            Console.Write("\n Mang giam dan: ");
+            xuat(manggiam);
         }
         public void SapXepTang()
         {
             int[] mangtang = new int[10];
             mangtang = mang;
-            for (int i = 0; i<10; i++)
-            {
-                for (int j = i + 1; j < 10; j++)
-                    if (mangtang[i] < mangtang[j])
-                    {
-                         int tg = mangtang[i];
-                         mangtang[i] = mangtang[j];
-                        mangtang[j] = tg;
-                    } 
-            }
-        }
-        public void SapXepGiam()
-        {
-            int[] manggiam = new int[10];
-            manggiam = mang;
             for (int i = 0; i < 10; i++)
             {
-                for (int j = i + 1; j < 10; j++)
-                    if (manggiam[i] > manggiam[j])
+                for (int j = i + 1; j <= 10; j++)
+                    if (mangtang[i] > mangtang[j])
                     {
-                        int tg = manggiam[i];
-                        manggiam[i] = manggiam[j];
-                        manggiam[j] = tg;
+                        int tg = mangtang[i];
+                        mangtang[i] = mangtang[j];
+                        mangtang[j] = tg;
                     }
             }
+            Console.Write("\n Mang tang dan: ");
+            xuat(mangtang);
         }
-
+        public void xuat(int[] mang)
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.Write(" | {0} | ", mang[i]);
+        }
 
         static void Main(string[] args)
         {
+            Program bt = new Program();
+            bt.nhap();
+            bt.min();
+            bt.max();
+            bt.XuatHienNhieu();
+            bt.SapXepGiam();
+            bt.SapXepTang();
             Console.ReadKey();
         }
     }
