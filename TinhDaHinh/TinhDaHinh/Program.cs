f3d2d80c90a681a191b85cc2e14e541bff193406
vanthanh <thanhjayz01@gmail.com>
2022-06-01 22:08:35 +0700

    tinh da hinh

---
+++ TinhDaHinh/TinhDaHinh/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TinhDaHinh
+{
+    class HinhDaGiac
+    {
+        string ten;
+        int soCanh;
+        public void tinhTien()
+        {
+            x'1 = x1 + dx;
+            x'2 = x2 + dx;
+            x'3 = x3 + dx;
+            y'1 = y1 + dy;
+            y'2 = y2 + dy;
+            y'3 = y3 + dy;
+        }
+    }
+    class HinhTamGiac : HinhDaGiac
+    {
+    }
+    class HinhTuGiac : HinhDaGiac
+    {
+    }
+    class HinhBinhHanh : HinhDaGiac
+    {
+    }
+    class HinhChuNhat : HinhDaGiac
+    {
+
+    }
+}
