ce302b35c0dfdb818ceee006987349462583ee9f
vanthanh <thanhjayz01@gmail.com>
2022-06-01 21:52:10 +0700

    Bai tap diem danh 26/4

---
+++ BTDiemDanh26-4/BTDiemDanh26-4/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTDiemDanh26_4
+{
+    public abstract class Animal
+    {
+        public abstract void eat();
+        public abstract void makeSound();
+    }
+    class Cat : Animal
+    {
+        public override void eat() { }
+        public override void makeSound() { }
+        public void run() { }
+    }
+    class Brid : Animal
+    {
+        public override void eat() {}
+        public override void makeSound() {}
+        public void fly() {}
+    }
+}
---
+++ BTDiemDanh26-4/BTDiemDanh26-4/PTBac2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTDiemDanh26_4
+{
+    class PTBac2
+    {
+        private int a, b, c;
+
+        public PTBac2()
+        {
+        }
+        public PTBac2(int a, int b, int c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        public int delta()
+        {
+            return b * b - 4 * a * c;
+        }
+        public void nghiem()
+        {
+            int d = delta();
+            Console.WriteLine("delta = {0}", d);
+            if(d<0)
+                Console.WriteLine("pt vo nghiem");
+            else if(d==0)
+                Console.WriteLine($"pt co 1 nghiem duy nhat x = {(-b*1.0/(2*a))}");
+            else
+                Console.WriteLine($"pt co 2 nghiem phan biet x1 = {((-b + Math.Sqrt(d*1.0)) / (2 * a))}, x2 = {((-b - Math.Sqrt(d * 1.0)) / (2 * a))}");
+        }
+
+        public int A { get => a; set => a = value; }
+        public int B { get => b; set => b = value; }
+        public int C { get => c; set => c = value; }
+
+        static void Main(string[] args)
+        {
+            PTBac2 pt257 = new PTBac2(2, -7, 3);
+            pt257.nghiem();
+            Console.ReadKey();
+        }
+    }
+}
