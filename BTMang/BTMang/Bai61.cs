07f28e5486f5647864006d33c0c0014fb097d80b
vanthanh <thanhjayz01@gmail.com>
2022-06-01 21:53:02 +0700

    bt mang

---
+++ BTMang/BTMang/Bai61.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BTMang
+{
+    class Bai61
+    {
+        static void Main(string[] args)
+        {
+            //cau a
+            int n, p, tong=0;
+            int[] mang;
+            Random random = new Random();
+            Console.WriteLine("Nhap n phan tu cua mang: ");
+            n = Int32.Parse(Console.ReadLine());
+            mang = new int[n+1];
+            for (int i =1; i<=n; i++)
+                mang[i] = random.Next(-101, 101);
+            for (int i = 1; i <= n; i++)
+                Console.Write(" | {0} | ", mang[i]);
+            Console.Write("\n");
+            //cau b
+            foreach (var x in mang)
+                if (x >= 0)
+                    tong += x;
+            Console.WriteLine("Tong cac so nguyen duong co trong mang = {0}", tong);
+            //cau c
+            Console.WriteLine("Nhap vi tri phan tu muon xoa: ");
+            p = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+                if (i >= p)
+                    mang[i] = mang[i + 1];
+            for (int i = 1; i <= n-1; i++)
+                Console.Write(" | {0} | ", mang[i]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BTMang/BTMang/Bai63.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTMang
+{
+    class Bai63
+    {
+        static void Main(string[] args)
+        {
+            //cau a
+            int n, count = 0;
+            int[] mang;
+            Random random = new Random();
+            Console.WriteLine("Nhap n phan tu cua mang: ");
+            n = Int32.Parse(Console.ReadLine());
+            mang = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+                mang[i] = random.Next(-101, 101);
+            for (int i = 1; i <= n; i++)
+                Console.Write(" | {0} | ", mang[i]);
+            Console.Write("\n");
+            //cau b
+            foreach (var x in mang)
+                if (x %10==6 && x%4 == 0)
+                    count ++;
+            Console.WriteLine("Co {0} phan tu chia het cho 4, tan cung 6", count);
+            //cau c
+            for (int i = 1; i <= n; i += 2)
+                mang[i] *= 2;
+            Console.WriteLine("Mang sau khi x2 phan tu le: ");
+            for (int i = 1; i <= n; i++)
+                Console.Write(" | {0} | ", mang[i]);
+            Console.ReadKey();
+        }
+    }
+}
